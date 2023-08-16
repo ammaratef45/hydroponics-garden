88780ccac0bea043be57c31b8ad73ff22ac775f5
Ammar Hussein <ammar.atef45@gmail.com>
2023-08-16 04:44:04 -0700

    Fixes #4: changed loging page design

--- lib/widgets/login_widget.dart
+++ lib/widgets/login_widget.dart
@@ -8,18 +8,75 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Auth.instance().signIn().then((value) {
-              Navigator.popAndPushNamed(context, PlantsPage.routeName);
-            });
-          },
-          child: const Text('login'),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+          Colors.green,
+          Colors.blue,
+        ])),
+        child: Card(
+          margin: const EdgeInsets.only(
+            top: 200,
+            bottom: 200,
+            left: 30,
+            right: 30,
+          ),
+          elevation: 20,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              const Text(
+                'Hydroponic Garden',
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 20,
+                  right: 20,
+                ),
+                child: MaterialButton(
+                  color: Colors.teal[100],
+                  elevation: 10,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Container(
+                        height: 30.0,
+                        width: 30.0,
+                        decoration: const BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage('assets/google_icon.png'),
+                              fit: BoxFit.cover),
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      const Text(
+                        'Sign In with Google',
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  onPressed: () {
+                    Auth.instance().signIn().then(
+                      (value) {
+                        Navigator.popAndPushNamed(
+                            context, PlantsPage.routeName);
+                      },
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
