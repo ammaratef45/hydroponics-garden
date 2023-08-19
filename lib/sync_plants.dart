93e3d6c77ad04581ba879e771a37977e15c29d83
Ammar Hussein <ammar.atef45@gmail.com>
2023-08-19 05:05:46 -0700

    #3 added more plants

--- lib/sync_plants.dart
+++ lib/sync_plants.dart
@@ -138,4 +138,75 @@ List<Map<String, dynamic>> values = [
     'sproutToHarvest': 40,
     'goodFor': 42
   },
+  {'name': 'Matilda', 'daysToSprout': 14, 'sproutToHarvest': 52, 'goodFor': 28},
+  {
+    'name': 'Monte Carlo',
+    'daysToSprout': 14,
+    'sproutToHarvest': 93,
+    'goodFor': 56
+  },
+  {
+    'name': 'Purslane',
+    'daysToSprout': 11,
+    'sproutToHarvest': 10,
+    'goodFor': 32
+  },
+  {
+    'name': 'Red Amaranth',
+    'daysToSprout': 9,
+    'sproutToHarvest': 110,
+    'goodFor': 25
+  },
+  {
+    'name': 'Red Mustard',
+    'daysToSprout': 13,
+    'sproutToHarvest': 33,
+    'goodFor': 35
+  },
+  {'name': 'Red Sail', 'daysToSprout': 9, 'sproutToHarvest': 40, 'goodFor': 42},
+  {
+    'name': 'Red Salad Bowl',
+    'daysToSprout': 8,
+    'sproutToHarvest': 45,
+    'goodFor': 45
+  },
+  {
+    'name': 'Red Sorrel',
+    'daysToSprout': 18,
+    'sproutToHarvest': 10,
+    'goodFor': 25
+  },
+  {'name': 'Romaine', 'daysToSprout': 9, 'sproutToHarvest': 68, 'goodFor': 28},
+  {
+    'name': 'Rouge d\'hiver',
+    'daysToSprout': 14,
+    'sproutToHarvest': 39,
+    'goodFor': 28
+  },
+  {'name': 'Sorrel', 'daysToSprout': 18, 'sproutToHarvest': 67, 'goodFor': 25},
+  {
+    'name': 'Swiss Chard',
+    'daysToSprout': 14,
+    'sproutToHarvest': 46,
+    'goodFor': 32
+  },
+  {'name': 'Tatsoi', 'daysToSprout': 13, 'sproutToHarvest': 33, 'goodFor': 32},
+  {
+    'name': 'Wasabi Greens',
+    'daysToSprout': 8,
+    'sproutToHarvest': 45,
+    'goodFor': 49
+  },
+  {
+    'name': 'Watercress',
+    'daysToSprout': 14,
+    'sproutToHarvest': 60,
+    'goodFor': 28
+  },
+  {
+    'name': 'Wheatgrass',
+    'daysToSprout': 6,
+    'sproutToHarvest': 21,
+    'goodFor': 45
+  },
 ];
